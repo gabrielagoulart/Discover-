0f5b0d6807e9cb316ef36882a482c740ce32bc4b
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-15 19:10:37 -0300

    Exercícios de JavaScript

---
+++ guia02/JS/exercicios.js
@@ -0,0 +1,84 @@
+// 1. Crie uma variável de nome weight
+let weight
+
+// 2. Que tipo de dado é a variável acima?
+console.log(typeof weight)
+// R: Undefined
+
+/* 3. Declare uma variável e atribua valores para cada um dos dados:
+    * name: String
+    * age: Number (integer)
+    * stars : Number (float)
+    * isSubscribed: Boolean
+*/
+let name = "Gabriela"
+let age = 15
+let stars = 4.8
+let isSubscribed = true
+
+/*
+    4. A variável student abaixo é de que tipo de dado?
+    let student = {}
+
+    console.log(typeof student)
+    R: Object
+
+
+    4.1 Attribua a ela as mesmas propriedades e valores do exercício 3.
+    let student = {
+        name: "mayk",
+        age: 23,
+        wight: 64.0,
+        isSubscribed: true,
+    }
+
+    
+    4.2 Mostre no console a seguinte mensagem:
+    console.log(`${student.name}, de ${student.age}, pesa ${student.weight} kg.`)   
+*/ 
+
+/*
+    5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio.
+
+*/
+let students = [
+
+]
+
+/* 
+    6. Reatribua um valor para a variáveel acima, colocando dentro dela o objeto da questão 4. 
+
+*/
+let students = [
+   student 
+]
+
+/* 7. Coloque no console o valor da posição zero do Array acima 
+    R: console.log(students[0])
+*/
+
+/* 8. Crie um novo student e coloque na posição 1 do Array students */
+const john = {
+  name: "John",
+  age: 23,
+  weight: 74.8,
+  isSubscribed: true,
+}
+
+students = [
+    student,
+    john
+]
+
+ ou 
+
+students[1] = john 
+
+/* 
+    9. Sem rodar o código, repsonda a resposta do código abaixo e por quê?
+
+    console.log(a)
+    var a = 1
+
+    R: O valor será undefined
+*/
