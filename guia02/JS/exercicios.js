74875f712e48758be8628644540578589df84cfe
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-15 21:27:57 -0300

    ajustes exercícios

--- guia02/JS/exercicios.js
+++ guia02/JS/exercicios.js
@@ -1,9 +1,12 @@
 // 1. Crie uma variável de nome weight
 let weight
 
+
 // 2. Que tipo de dado é a variável acima?
 console.log(typeof weight)
 // R: Undefined
+
+
 
 /* 3. Declare uma variável e atribua valores para cada um dos dados:
     * name: String
@@ -16,7 +19,10 @@ let age = 15
 let stars = 4.8
 let isSubscribed = true
 
+
 /*
+
+
     4. A variável student abaixo é de que tipo de dado?
     let student = {}
 
@@ -37,6 +43,8 @@ let isSubscribed = true
     console.log(`${student.name}, de ${student.age}, pesa ${student.weight} kg.`)   
 */ 
 
+
+
 /*
     5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio.
 
@@ -46,6 +54,8 @@ let students = [
 ]
 
 /* 
+
+
     6. Reatribua um valor para a variáveel acima, colocando dentro dela o objeto da questão 4. 
 
 */
@@ -53,9 +63,13 @@ let students = [
    student 
 ]
 
+
+
 /* 7. Coloque no console o valor da posição zero do Array acima 
     R: console.log(students[0])
 */
+
+
 
 /* 8. Crie um novo student e coloque na posição 1 do Array students */
 const john = {
@@ -72,7 +86,8 @@ students = [
 
  ou 
 
-students[1] = john 
+students[1] = john
+
 
 /* 
     9. Sem rodar o código, repsonda a resposta do código abaixo e por quê?
