c338e86d94efc0600928e5a541602bde072137a6
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-16 11:57:31 -0300

    Arrow Function

---
+++ guia02/JS/arrowfunction.js
@@ -0,0 +1,10 @@
+const SayMyName = (name) => {
+    console.log(name)
+}
+
+SayMyName('Gabi')
+
+
+//substítui a palavra function pela =>
+//o "name" entre parentêses é um parâmetro
+//o "Gabi" entre parentêses é um argumento
